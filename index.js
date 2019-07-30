de398a7875d80ee9c52e3552e87c29b7eaa79213
energetic-port-0799 <m.tarrats@yahoo.com>
2019-07-30 23:10:35 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,41 @@
+a = Math.floor(Math.random() * 1000)
+b = Math.floor(Math.random() * 1000)
+
+function add(a,b){
+  return (a + b)
+}
+
+a = Math.floor(Math.random() * 1000)
+b = Math.floor(Math.random() * 1000)
+
+function subtract(a,b){
+  return (a - b)
+}
+
+a = Math.floor(Math.random() * 1000)
+b = Math.floor(Math.random() * 1000)
+
+function multiply(a,b){
+  return (a * b)
+}
+
+a = Math.floor(Math.random() * 1000)
+b = Math.floor(Math.random() * 1000)
+
+function divide(a,b){
+  return (a / b)
+}
+
+a = Math.floor(Math.random() * 1000)
+b = Math.floor(Math.random() * 1000)
+
+function add(a,b){
+  return (a + 1)
+}
+
+n = Math.floor(Math.random() * 1000)
+
+function makeInt(n){
+  return parseInt("n")
+
+}
